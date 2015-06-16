8afa8d9738ab4723671b0e10a1146b3fc89cb1e6
Thibaud Arguillere <targuillere@nuxeo.com>
2015-06-16 19:07:25 -0400

    Unit testing

--- nuxeo-diff-pictures-plugin/src/test/java/org/nuxeo/diff/pictures/test/DiffPicturesTest.java
+++ nuxeo-diff-pictures-plugin/src/test/java/org/nuxeo/diff/pictures/test/DiffPicturesTest.java
@@ -19,9 +19,13 @@ package org.nuxeo.diff.pictures.test;
 
 import static org.junit.Assert.*;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.HashMap;
+
+import javax.imageio.ImageIO;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -55,6 +59,14 @@ public class DiffPicturesTest {
 
     protected static final Log log = LogFactory.getLog(DiffPicturesTest.class);
 
+    protected static final String ISLAND_PNG = "island.png";
+
+    protected static final int ISLAND_W = 400;
+
+    protected static final int ISLAND_H = 282;
+
+    protected static final String ISLAND_MODIF_PNG = "island-modif.png";
+
     @Inject
     CoreSession coreSession;
 
@@ -71,8 +83,8 @@ public class DiffPicturesTest {
     @Test
     public void testDefaultValues() throws Exception {
 
-        File img1 = FileUtils.getResourceFileFromContext("island.png");
-        File img2 = FileUtils.getResourceFileFromContext("island-modif.png");
+        File img1 = FileUtils.getResourceFileFromContext(ISLAND_PNG);
+        File img2 = FileUtils.getResourceFileFromContext(ISLAND_MODIF_PNG);
 
         FileBlob blob1 = new FileBlob(img1);
         FileBlob blob2 = new FileBlob(img2);
@@ -81,18 +93,18 @@ public class DiffPicturesTest {
         DiffPictures dp = new DiffPictures(blob1, blob2);
 
         result = dp.compare(null, null);
-        assertNotNull(result);
-        assertTrue(result instanceof FileBlob);
-        ((FileBlob) result).getFile().delete();
+
+        checkIsImage(result, ISLAND_W, ISLAND_H);
+        deleteFile(result);
 
     }
 
     @Test
     public void testFuzz() throws Exception {
 
-        File img1 = FileUtils.getResourceFileFromContext("island.png");
+        File img1 = FileUtils.getResourceFileFromContext(ISLAND_PNG);
         long len1 = img1.length();
-        File img2 = FileUtils.getResourceFileFromContext("island-modif.png");
+        File img2 = FileUtils.getResourceFileFromContext(ISLAND_MODIF_PNG);
         File aFile;
 
         FileBlob blob1 = new FileBlob(img1);
@@ -108,11 +120,55 @@ public class DiffPicturesTest {
         params = new HashMap<String, Serializable>();
         params.put("fuzz", "10%");
         result = dp.compare(null, params);
-        assertNotNull(result);
-        assertTrue(result instanceof FileBlob);
+
+        checkIsImage(result, ISLAND_W, ISLAND_H);
+
         aFile = ((FileBlob) result).getFile();
-        assertTrue("Result image with fuzz should be lower than original", aFile.length() < (len1 /2));
-        aFile.delete();
+        assertTrue("Result image with fuzz should be lower than original",
+                aFile.length() < (len1 / 2));
+
+        deleteFile(result);
+
+    }
+
+    protected void checkIsImage(Blob inResult, int inExpectedW, int expectedH)
+            throws Exception {
+
+        assertTrue(inResult instanceof FileBlob);
+        checkIsImage((FileBlob) inResult, inExpectedW, expectedH);
+    }
+
+    protected void checkIsImage(FileBlob inResult, int inExpectedW,
+            int expectedH) throws Exception {
+
+        assertNotNull(inResult);
+
+        File f = inResult.getFile();
+        assertNotNull(f);
+        assertTrue(f.length() > 0);
+
+        BufferedImage bi;
+        try {
+            bi = ImageIO.read(f);
+            assertNotNull(bi);
+
+            assertEquals(inExpectedW, bi.getWidth());
+            assertEquals(expectedH, bi.getHeight());
+
+        } catch (IOException e) {
+            throw new Exception("Error reading the file", e);
+        }
+
+    }
+
+    protected void deleteFile(Blob inBlob) {
+
+        if (inBlob instanceof FileBlob) {
+            File f = inBlob.getFile();
+            if (f != null) {
+                f.delete();
+            }
+        }
 
     }
 
